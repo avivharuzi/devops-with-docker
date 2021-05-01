3aa2b83972faf198089c89fe75b58ffaf854dd2f
Aviv Haruzi <avivharuzi@gmail.com>
2021-05-01 09:03:35 +0300

    Update docker version

--- install-docker.sh
+++ install-docker.sh
@@ -5,10 +5,10 @@ curl -sSL https://get.docker.com/ | sh
 sudo docker version
 
 # Install Docker Machine
-base=https://github.com/docker/machine/releases/download/v0.14.0 && curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine && sudo install /tmp/docker-machine /usr/local/bin/docker-machine
+base=https://github.com/docker/machine/releases/download/v0.16.2 && curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine && sudo install /tmp/docker-machine /usr/local/bin/docker-machine
 sudo docker-machine version
 
 # Install Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo docker-compose version
